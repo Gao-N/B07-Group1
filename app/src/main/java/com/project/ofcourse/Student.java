6d2f8cb17017e47472f4c1524de13b986a60f593
Tasif <tasif006@gmail.com>
2022-11-24 23:16:05 -0500

    minor UI changes

--- app/src/main/java/com/project/ofcourse/Student.java
+++ app/src/main/java/com/project/ofcourse/Student.java
@@ -2,11 +2,11 @@ package com.project.ofcourse;
 
 import androidx.annotation.NonNull;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
 
 public class Student {
     String email;
