18ea693c3c3dcddd22e1178699ecec0dd7735e6c
cassy18 <cassy.dias@mail.utoronto.ca>
2022-12-01 17:25:20 -0500

    Added a current session editText and functionality, merged Darren and Cassandra

--- app/src/main/java/com/project/ofcourse/AdminDashboard.java
+++ app/src/main/java/com/project/ofcourse/AdminDashboard.java
@@ -4,10 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AdminDashboard extends AppCompatActivity {
+    EditText session;
+    Button edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +49,45 @@ public class AdminDashboard extends AppCompatActivity {
                 viewAdminLogin();
             }
         });
+        session = findViewById(R.id.currentSession);
+        edit = findViewById(R.id.btnSession);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] arr = session.getText().toString().trim().split(" ");
+                String[] sess = {"F", "W", "S"};
+                if (session.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Session Cannot be Empty",
+                            Toast.LENGTH_LONG).show();
+                }
+                else if (arr.length > 2 || !(Arrays.asList(sess)).contains(arr[0])) {
+                    Toast.makeText(getApplicationContext(), "Invalid Session Format",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("currentSession", session.getText().toString());
+                    FirebaseDatabase.getInstance().getReference().child("admin")
+                            .updateChildren(map)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(getApplicationContext(), "Session Updated Successfully",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getApplicationContext(), "Error Updating Session",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
+            }
+        });
     }
+
+
     public void openAdminAddCourse(){
         Intent intent = new Intent(this, AdminAddCourse.class);
         startActivity(intent);
--- app/src/main/java/com/project/ofcourse/CourseList.java
+++ app/src/main/java/com/project/ofcourse/CourseList.java
@@ -1,52 +1,38 @@
 package com.project.ofcourse;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.content.Intent;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.auth.User;
 
 import java.util.ArrayList;
 
 public class CourseList extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    FirebaseFirestore db;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     MyAdapter myAdapter;
     ArrayList<Course> list;
-    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courselist);
 
-        ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("Retrieving Course Data...");
-        progressDialog.show();
-
         recyclerView = findViewById(R.id.courseList);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        db = FirebaseFirestore.getInstance();
         list = new ArrayList<Course>();
         myAdapter = new MyAdapter(this, list);
         recyclerView.setAdapter(myAdapter);
@@ -60,9 +46,6 @@ public class CourseList extends AppCompatActivity {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                         if (error != null){
-                            if (progressDialog.isShowing()){
-                                progressDialog.dismiss();
-                            }
                             Log.e("Firestore error", error.getMessage());
                             return;
                         }
@@ -70,12 +53,7 @@ public class CourseList extends AppCompatActivity {
                         for (DocumentChange dc : value.getDocumentChanges()){
                             list.add(dc.getDocument().toObject(Course.class));
                         }
-
                         myAdapter.notifyDataSetChanged();
-                        if (progressDialog.isShowing()){
-                            progressDialog.dismiss();
-                        }
-
                     }
                 });
     }
